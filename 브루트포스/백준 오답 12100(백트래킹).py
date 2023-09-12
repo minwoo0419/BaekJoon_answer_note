dbedd7ed69323dd924eab628ad7bdfb5e277631b
minwoo0419 <ghdl464@naver.com>
2023-09-13 00:31:00 +0900

    브루트포스 추가

---
+++ 브루트포스/백준 오답 12100(백트래킹).py
@@ -0,0 +1,108 @@
+from copy import deepcopy
+
+
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+def input_arr(count):
+    graph = []
+    for i in range(count):
+        graph.append(list(map(int, input().split())))
+
+    return graph
+    
+
+def move(arr, x, y, count):
+    if (x == 0 and y == 1): # 동쪽
+        for i in range(count):
+            top = count - 1
+            for j in range(count-2, -1, -1):
+                if (arr[i][j] != 0):
+                    tmp = arr[i][j]
+                    arr[i][j] = 0
+                    if arr[i][top] == 0:
+                        arr[i][top] = tmp
+                    elif arr[i][top] == tmp:
+                        arr[i][top] = tmp * 2
+                        top -= 1
+                    else:
+                        top -= 1
+                        arr[i][top] = tmp
+                        
+                
+    elif (x == 0 and y == -1): # 서쪽
+        for i in range(count):
+            top = 0
+            for j in range(1, count):
+                if (arr[i][j] != 0):
+                    tmp = arr[i][j]
+                    arr[i][j] = 0
+                    if arr[i][top] == 0:
+                        arr[i][top] = tmp
+                    elif arr[i][top] == tmp:
+                        arr[i][top] = tmp * 2
+                        top += 1
+                    else:
+                        top += 1
+                        arr[i][top] = tmp
+    elif (x == 1 and y == 0): # 남쪽
+        for j in range(count):
+            top = count - 1
+            for i in range(count-2, -1, -1):
+                if (arr[i][j] != 0):
+                    tmp = arr[i][j]
+                    arr[i][j] = 0
+                    if arr[top][j] == 0:
+                        arr[top][j] = tmp
+                    elif arr[top][j] == tmp:
+                        arr[top][j] = tmp * 2
+                        top -= 1
+                    else:
+                        top -= 1
+                        arr[top][j] = tmp
+    else: # 북쪽
+        for j in range(count):
+            top = 0
+            for i in range(1, count):
+                if (arr[i][j] != 0):
+                    tmp = arr[i][j]
+                    arr[i][j] = 0
+                    if arr[top][j] == 0:
+                        arr[top][j] = tmp
+                    elif arr[top][j] == tmp:
+                        arr[top][j] = tmp * 2
+                        top += 1
+                    else:
+                        top += 1
+                        arr[top][j] = tmp
+    return arr
+
+def get_max(arr, count):
+    max = 0
+    for i in range(count):
+        for j in range(count):
+            if (max < arr[i][j]):
+                max = arr[i][j]
+    return max
+
+global max
+max = 0
+
+def simulation(arr, tmp, count):
+    global max
+    if (tmp == 5):
+        if (max < get_max(arr, count)):
+            max = get_max(arr, count)
+        return
+    for i in range(4):
+        tmp_arr = move(deepcopy(arr), dx[i], dy[i], count) # deepcopy를 안하면 원본 배열이 바뀜
+        simulation(tmp_arr, tmp + 1, count)
+
+def main():
+    count = int(input())
+    arr = input_arr(count)
+    simulation(arr, 0, count)
+    print(max)
+
+if __name__ == "__main__":
+    main()
