40568917a5ad8f4ce64c71710cdbb187db570386
Minwoo Cho <103352114+minwoo0419@users.noreply.github.com>
2024-03-26 15:02:43 +0900

    [Gold V] Title: 전깃줄, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/2565. 전깃줄/전깃줄.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    int x, y;
+    int cnt = 0;
+    cin >> n;
+    vector <int> dp(n, 0);
+    vector < pair <int, int> > a;
+    for (int i = 0 ; i < n ; i++)
+    {
+        cin >> x;
+        cin >> y;
+        a.push_back(make_pair(x, y));
+    }
+    sort(a.begin(), a.end());
+    for (int i = 0 ; i < n ; i++)
+    {
+        dp[i] = 1;
+        for (int j = 0 ; j < i ; j++)
+        {
+            if (a[i].second > a[j].second)
+                dp[i] = max(dp[i], dp[j] + 1);
+        }
+        cnt = max(dp[i], cnt);
+    }
+    cout << (n - cnt);
+    return (0);
+}
