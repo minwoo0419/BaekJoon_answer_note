9a3887765eb64a8bea2fecfe9a5ba3e3d49d3e6c
Minwoo Cho <103352114+minwoo0419@users.noreply.github.com>
2024-06-02 19:52:35 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 468 ms, Memory: 211908 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.cc
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.cc
@@ -1,24 +1,19 @@
 #include<iostream>
 #include<vector>
 #include<map>
+#include<set>
 #include<stack>
 #include<sstream>
 using namespace std;
-map<string, int> str;
+set<string> str;
 string input;
-int count = 0;
-void func(int idx, string s){
-    if (idx >= input.size())
-        return;
-    string tmp = s + input[idx];
-    str[tmp]++;
-    func(idx + 1, tmp);
-}
 int main(int argc, char** argv)
 {
     cin >> input;
     for (int i = 0 ; i < input.length() ; i++){
-        func(i, "");
+        for (int j = 1 ; j <= input.length() - i ; j++){
+            str.insert(input.substr(i, j));
+        }
     }
     cout << str.size();
     return 0;
