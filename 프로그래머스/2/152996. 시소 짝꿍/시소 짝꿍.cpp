01595786b416f3bb519f22b9fefb83ecab19a0a5
Minwoo Cho <103352114+minwoo0419@users.noreply.github.com>
2024-08-22 15:56:57 +0900

    [level 2] Title: 시소 짝꿍, Time: 2.60 ms, Memory: 6.5 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.cpp
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.cpp
@@ -7,23 +7,21 @@ using namespace std;
 
 long long solution(vector<int> weights) {
     long long answer = 0;
+    int mul[3] = {3, 2, 4};
+    int div[3] = {2, 1, 3};
     unordered_map<long long, long long> ans;
     for (int i = 0 ; i < weights.size() ; i++){
         ans[weights[i]]++;
     }
     for (auto p : ans){
-        if (p.second){
+        if (p.second)
             answer += p.second * (p.second - 1) / 2;
+        for (int i = 0 ; i < 3 ; i++){
+            if (p.first % div[i] == 0){
+                if (ans.find(p.first / div[i] * mul[i]) != ans.end())
+                    answer += ans[p.first / div[i] * mul[i]] * p.second;
+            }
         }
-        if (p.first % 2 == 0){
-            if (ans.find(p.first * 3 / 2) != ans.end())
-                answer += ans[p.first * 3 / 2] * p.second;
-        } if (p.first % 3 == 0){
-            if (ans.find(p.first * 4 / 3) != ans.end())
-                answer += ans[p.first * 4 / 3] * p.second;
-        }
-        if (ans.find(p.first * 2) != ans.end())
-            answer += ans[p.first * 2] * p.second;
     }
     return answer;
 }
