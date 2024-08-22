6132286c79e0089875357939d2cb878bcf4277ae
Minwoo Cho <103352114+minwoo0419@users.noreply.github.com>
2024-08-22 13:35:10 +0900

    [level 2] Title: 큰 수 만들기, Time: 1493.55 ms, Memory: 5.46 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -5,26 +5,18 @@ using namespace std;
 
 string solution(string number, int k) {
     string answer = "";
-    int t = 0, idx = 0, s = -1;
-    while(idx < number.length()){
-        if (t < k){
-            int j = (k - t < answer.length()) ? answer.length() - k + t : 0;
-            while(j < answer.length()){
-                if (answer[j] < number[idx])
-                    break;
-                j++;
-            }
-            if (j < answer.length()){
-                s = j;
-                t += (answer.length() - s);
+    int idx = 0;
+    for (int i = 0 ; i < number.size() - k ; i++){
+        int maxNum = number[idx];
+        int maxIdx = idx;
+        for (int j = idx ; j <= i + k ; j++){
+            if (maxNum < number[j]){
+                maxIdx = j;
+                maxNum = number[j];
             }
         }
-        if (s != -1){
-            answer = answer.substr(0, s) + number[idx++];
-            s = -1;
-        } else
-            answer += number[idx++];
+        idx = maxIdx + 1;
+        answer += maxNum;
     }
-    answer = answer.substr(0, answer.length() - k + t);
     return answer;
 }
