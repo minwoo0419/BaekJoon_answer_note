39a86a46353875a4b2e443a04aa0688a9a3ef7af
Minwoo Cho <103352114+minwoo0419@users.noreply.github.com>
2024-10-01 16:11:24 +0900

    [level 2] Title: 롤케이크 자르기, Time: 271.61 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -3,26 +3,20 @@
 #include <map>
 #include <iostream>
 using namespace std;
-
 int solution(vector<int> topping) {
     int answer = 0;
     map<int, int> m1, m2;
+    int a1 = 0, a2 = 0;
     for (int i = 0 ; i < topping.size() ; i++){
         m2[topping[i]]++;
+        if (m2[topping[i]] == 1) a2++;
     }
-    int c1 = 0, c2 = m2.size();
     for (int i = 0 ; i < topping.size() ; i++){
-        m1[topping[i]]++;
         m2[topping[i]]--;
-        if(m1[topping[i]] == 1)
-            c1++;
-        if(m2[topping[i]] == 0)
-            c2--;
-        if(c1 == c2)
-            answer++;
-        else if (c1 > c2)
-            break;
-        
+        m1[topping[i]]++;
+        if (m2[topping[i]] == 0) a2--;
+        if (m1[topping[i]] == 1) a1++;
+        if (a2 == a1) answer++;
     }
     return answer;
 }
