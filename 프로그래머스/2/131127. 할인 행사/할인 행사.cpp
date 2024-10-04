d741064ed8251c3b04d98e18267c9412785828bb
Minwoo Cho <103352114+minwoo0419@users.noreply.github.com>
2024-10-04 13:04:49 +0900

    [level 2] Title: 할인 행사, Time: 19.96 ms, Memory: 8.79 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -6,21 +6,19 @@
 using namespace std;
 
 int solution(vector<string> want, vector<int> number, vector<string> discount) {
-    map<string, int> m;
+    map<string, int> m, m2;
     int answer = 0;
     for (int i = 0 ; i < want.size() ; i++){
         m[want[i]] = number[i];
     }
-    for (int i = 0 ; i <= discount.size() - 10 ; i++){
-        int flag = 1;
-        for (int j = 0 ; j < want.size() ; j++){
-            if (count(discount.begin() + i, discount.begin() + i + 10, want[j]) < number[j]){
-                flag = 0;
-                break;
-            }
-        }
-        if (flag)
-            answer++;
+    for (int i = 0 ; i < 9 ; i++){
+        m2[discount[i]]++;
+    }
+    for (int i = 9 ; i < discount.size() ; i++){
+        m2[discount[i]]++;
+        if (m == m2) answer++;
+        m2[discount[i-9]]--;
+        if (m2[discount[i-9]] ==0) m2.erase(discount[i - 9]);
     }
     return answer;
 }
