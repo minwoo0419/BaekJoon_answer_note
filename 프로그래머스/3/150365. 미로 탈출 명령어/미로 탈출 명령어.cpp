9e04d1cc3b4e2a0e70320b1e4a4e6401d7775d2f
Minwoo Cho <103352114+minwoo0419@users.noreply.github.com>
2024-10-04 19:09:15 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.04 ms, Memory: 4.03 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.cpp
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.cpp
@@ -5,40 +5,40 @@
 #include <iostream>
 using namespace std;
 
-void sum_str(string &s, string c, int cnt){
-    for (int i = 0 ; i < cnt ; i++){
+string link(string s, string c, int a){
+    for (int i = 0 ; i < a ; i++){
         s += c;
     }
+    return s;
 }
 
 string solution(int n, int m, int x, int y, int r, int c, int k) {
     string answer = "";
-    int a = abs(x - r) + abs(y - c);
-    k -= a;
-    if (k < 0 || k % 2) //최소거리보다 작거나 k - 최소 거리가 짝수이면 불가능
-        return "impossible";
-    int dic[4] = {0, 0, 0, 0};
-    if (r > x)
-        dic[0] += r - x;
-    else
-        dic[3] += x - r;
-    if (c > y)
-        dic[2] += c - y;
-    else
-        dic[1] += y - c;
-    //최소 거리일 때 카운팅
-    sum_str(answer, "d", dic[0]);
-    int t = min(k / 2, n - (x + dic[0]));   //남은 거리 중 d를 더 넣을 수 있는지
-    sum_str(answer, "d", t);
-    dic[3] += t;    //d만큼 u도 넣어주기
-    k -= (t * 2);
-    sum_str(answer, "l", dic[1]);   //남은 거리 중 l을 더 넣을 수 있는지
-    int h = min(k / 2, y - dic[1] - 1);
-    sum_str(answer, "l", h);
-    dic[2] += h;    //l만큼 r도 넣어주기
-    k -= (h * 2);
-    sum_str(answer, "rl", k / 2);   //남은 건 rl로 채우기
-    sum_str(answer, "r", dic[2]);
-    sum_str(answer, "u", dic[3]);
+    vector<int> arr(4, 0);
+    int rem = k - abs(x - r) - abs(y - c); //도착했는데 남은 거리
+    if (rem < 0 || rem % 2) return "impossible"; //음수거나 나머지 1이면 안됨
+    if (x < r) {
+        arr[0] += (r - x);
+    } else {
+        arr[3] += (x - r);
+    }
+    if (c < y) {
+        arr[1] += (y - c);
+    } else {
+        arr[2] += (c - y);
+    }
+    int canDown = min(rem / 2, n - (x + arr[0]));
+    arr[0] += canDown;
+    arr[3] += canDown;
+    rem -= canDown * 2;
+    int canLeft = min(rem / 2, y - arr[1] - 1);
+    arr[1] += canLeft;
+    arr[2] += canLeft;
+    rem -= canLeft * 2;
+    answer = link(answer, "d", arr[0]);
+    answer = link(answer, "l", arr[1]);
+    answer = link(answer, "rl", rem / 2);
+    answer = link(answer, "r", arr[2]);
+    answer = link(answer, "u", arr[3]);
     return answer;
 }
