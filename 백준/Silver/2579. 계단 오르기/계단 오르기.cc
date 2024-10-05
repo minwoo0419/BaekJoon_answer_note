5e05e67643c32be4f537af17e0fa92d88763098a
Minwoo Cho <103352114+minwoo0419@users.noreply.github.com>
2024-10-05 23:39:45 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -14,19 +14,12 @@ int main()
         a.push_back(x);
         dp.push_back(0);
     }
-    int sum = 0;
-    for (int i = 0 ; i < n ; i++)
+    dp[0] = a[0];
+    dp[1] = a[0] + a[1];
+    dp[2] = max(a[0], a[1]) + a[2];
+    for (int i = 3 ; i < n ; i++)
     {
-        if (i == 0)
-            dp[i] = a[i];
-        else if (i == 1)
-            dp[i] = a[i] + dp[0];
-        else if (i == 2)
-        {
-            dp[i] = max(a[i]+a[i-1], a[i]+a[i-2]);
-        }
-        else
-            dp[i] = max(dp[i-2] + a[i], dp[i-3] + a[i-1] + a[i]);
+        dp[i] = max(a[i] + dp[i-3] + a[i-1], a[i] + dp[i-2]);
     }
     cout << dp[n-1];
     return (0);
