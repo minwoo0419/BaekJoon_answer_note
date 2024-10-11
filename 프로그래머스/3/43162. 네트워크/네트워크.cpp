6b991547827a2b8b827692804b8ecb0d1c6ce876
Minwoo Cho <103352114+minwoo0419@users.noreply.github.com>
2024-10-11 21:47:19 +0900

    [level 3] Title: 네트워크, Time: 0.07 ms, Memory: 4.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -7,29 +7,28 @@ using namespace std;
 vector<int> parent;
 
 int find_parent(int n){
-    if (parent[n] == n) return n;
+    if (parent[n] < 0) return n;
     return parent[n] = find_parent(parent[n]);
 }
-void union_find(int a, int b){
+
+void merge(int a, int b){
     a = find_parent(a);
     b = find_parent(b);
     if (a == b) return ;
-    if (a > b) swap(a, b);
+    if ( a > b) swap(a, b);
     parent[b] = a;
 }
 
 int solution(int n, vector<vector<int>> computers) {
-    for (int i = 0 ; i < n ; i++){
-        parent.push_back(i);
-    }
+    int answer = 0;
+    parent.assign(n, -1);
     for (int i = 0 ; i < n ; i++){
         for (int j = 0 ; j < n ; j++){
-            if (find_parent(i) == find_parent(j)) continue;
-            if (!computers[i][j]) continue;
-            union_find(i, j);
+            if(computers[i][j]) merge(i, j);
         }
     }
-    unordered_set<int> sets;
-    for (int i = 0 ; i < n ; i++) sets.insert(parent[i]);
-    return sets.size();
+    for (int i = 0 ; i < n ; i++){
+        if (parent[i] < 0) answer++;
+    }
+    return answer;
 }
