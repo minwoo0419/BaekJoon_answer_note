3f5db33515679eda0fd6df4d16af04c7ead6be4c
Minwoo Cho <103352114+minwoo0419@users.noreply.github.com>
2024-10-11 21:09:06 +0900

    [level 2] Title: H-Index, Time: 0.06 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -5,12 +5,11 @@
 using namespace std;
 
 int solution(vector<int> citations) {
-    int answer = 0;
-    sort(citations.begin(), citations.end());
-    for (int i = 0 ; i < citations.size() ; i++){
-        if(citations[i] >= citations.size() - i){
-            answer = citations.size() - i;
-            break;
+    int answer = 0, n = citations.size();
+    sort(citations.rbegin(), citations.rend());
+    for (int i = 0 ; i < n ; i++){
+        if (citations[i] >= i + 1) {
+            answer = i + 1;
         }
     }
     return answer;
